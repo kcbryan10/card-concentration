ce32163b5485df1927c316344fce7e445e9eea4e
kcbryan10 <kcbryan10@gmail.com>
2022-03-09 13:11:58 -0600

    updated app.js for spa application

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,25 @@
 import React from "react";
+import { BrowerRouter as Router, Route, Switch } from 'react-router-dom';
+
 import { ChakraProvider } from '@chakra-ui/react'
+
 import Header from "./components/Header";
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <ChakraProvider>
-
-      <Header />
-      <div className="page">
-      </div>
-      <Footer />
-
-    </ChakraProvider>
+    <Router>
+      <ChakraProvider>
+        <Header />
+        <div>
+          <Switch>
+            <Route exact path="/" component={Main}/>
+            <Route exact path="/leaderboard" component={Leaderboard}/>
+          </Switch>
+        </div>
+        <Footer />
+      </ChakraProvider>
+    </Router>
   );
 }
 
