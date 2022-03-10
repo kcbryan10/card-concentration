420d56f819d8160d2e7f052d70a05598ae330d4f
kcbryan10 <kcbryan10@gmail.com>
2022-03-09 20:38:23 -0600

    added apollo provider to app function

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
 import { ChakraProvider } from '@chakra-ui/react'
+import { setContext } from '@apollo/client/link/context';
 
 import Header from "./components/Header";
 import Footer from './components/Footer';
@@ -10,23 +10,45 @@ import Signup from './pages/Signup';
 
 import Main from './pages/Main';
 import Leaderboard from './pages/Leaderboard';
+import { ApolloClient, ApolloProvider, createHttpLink, HttpLink, InMemoryCache } from "@apollo/client";
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+})
+
+const authLink = setContext((_, {headers}) => {
+  const token = localStorage.getItem('id_token');
+  return { 
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}`: '',
+    },
+  }
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
 
 function App() {
   return (
-    <Router>
-      <ChakraProvider>
-        <Header />
-        <div>
-          <Switch>
-            <Route exact path="/" component={Main}/>
-            <Route exact path="/leaderboard" component={Leaderboard}/>
-            <Route exact path="/login" component={Login}/>
-            <Route exact path="/signup" component={Signup}/>
-          </Switch>
-        </div>
-        <Footer />
-      </ChakraProvider>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <ChakraProvider>
+          <Header />
+          <div>
+            <Switch>
+              <Route exact path="/" component={Main} />
+              <Route exact path="/leaderboard" component={Leaderboard} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+            </Switch>
+          </div>
+          <Footer />
+        </ChakraProvider>
+      </Router>
+    </ApolloProvider>
   );
 }
 
