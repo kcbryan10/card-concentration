ce5ff481dc45302828d1c4d8158c238d0101d1b7
kcbryan10 <kcbryan10@gmail.com>
2022-03-10 20:39:44 -0600

    added functionality to display cards and shuffle them randomly in a grid format

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,34 +1,44 @@
 import React, { useState } from 'react';
 
-const cardImg = [ 
- "poop",
- "fart",
- "stink",
- "butt",
- "turd",
- "toilet"
+const cardImg = [
+  {"src": "/img/bag.png"},
+  {"src": "/img/clock.png"},
+  {"src": "/img/diploma.png"},
+  {"src": "/img/palette.png"},
+  {"src": "/img/Pen.png"},
+  {"src": "/img/trophy.png"}
 ]
 
 const Main = () => {
   const [cards, setCards] = useState([])
   const [rounds, setRounds] = useState(0)
 
-const shuffle = () => {
-  const shuffled= [...cardImg, ...cardImg]
-  .sort(()=> Math.random() - 0.5)
-  .map((card) => ({...card, id: Math.random() }))
+  const shuffle = () => {
+    const shuffled = [...cardImg, ...cardImg]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }))
 
-  setCards(shuffled)
-  setRounds(0)
-}
+    setCards(shuffled)
+    setRounds(0)
+  }
 
-console.log(cards, rounds)
+  console.log(cards, rounds)
 
   return (
     <main className='game-page'>
       <div className='main'>
         <h1>Card Concentration</h1>
         <button onClick={shuffle}>Play Again</button>
+        <div className='game'>
+          {cards.map(card => (
+            <div className='card' key={card.id}>
+              <div>
+                <img className='icon' src={card.src}/>
+                <img className='card-background' src=""/>
+              </div>
+            </div>
+          ))}
+        </div>
         <div />
       </div>
     </main>
