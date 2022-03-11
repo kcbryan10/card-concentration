7c57078f1536a2a4f8eb6c35ff94a918f5177b7b
kcbryan10 <kcbryan10@gmail.com>
2022-03-10 20:21:09 -0600

    feature to shuffle cards in an array created

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const cardImg = [ 
+ "poop",
+ "fart",
+ "stink",
+ "butt",
+ "turd",
+ "toilet"
+]
 
 const Main = () => {
+  const [cards, setCards] = useState([])
+  const [rounds, setRounds] = useState(0)
+
+const shuffle = () => {
+  const shuffled= [...cardImg, ...cardImg]
+  .sort(()=> Math.random() - 0.5)
+  .map((card) => ({...card, id: Math.random() }))
+
+  setCards(shuffled)
+  setRounds(0)
+}
+
+console.log(cards, rounds)
+
   return (
     <main className='game-page'>
       <div className='main'>
         <h1>Card Concentration</h1>
-        <button>Play Again</button>
+        <button onClick={shuffle}>Play Again</button>
         <div />
       </div>
     </main>
